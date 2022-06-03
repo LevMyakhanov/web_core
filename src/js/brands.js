2b39255a474ffa51be9ef02d1d63687af3ac46b3
Lev <mynameislev@mail.ru>
2022-06-03 21:05:12 +0800

    правки1

--- src/js/brands.js
+++ src/js/brands.js
@@ -9,32 +9,29 @@
 let buttons = document.querySelectorAll(".buttons-show-hide");
 
 export let listenBrand = function (number) {
+    
     buttons.forEach(button => {
         button.addEventListener('click', function () {
             let brandList = button.parentElement.querySelector('.brands__list')
             brandList.classList.toggle("brands")
             brandList.classList.toggle("brands__hidden")
             
-
-            let textShowHide = function () {
+            
+            function ww () {
                 let brandTextNext = button.querySelector(".button-brands_show");
                 let brandTextHide = button.querySelector(".button-brands_hide");
                 if (number === 1) {
                     brandTextNext.style.display = 'none';
                     brandTextHide.style.display = 'block';
-                    number = 0
-                    return number;
+                    return number = 0
                 } 
                 if (number === 0) {
                     brandTextNext.style.display = 'block';
                     brandTextHide.style.display = 'none';
-                    number = 1;
-                    return number;
+                    return number = 1;
                 }
             }
-            
-            textShowHide()
-            
+            ww();
         })
     });
 }
